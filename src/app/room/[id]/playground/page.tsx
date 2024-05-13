d0384557e7f26e8dd7333ea6f36cbb93c294e1ba
hoaikhaqn <hoaikhaqn1996@gmail.com>
2024-05-13 09:36:11 +0700

    up code

--- src/app/room/[id]/playground/page.tsx
+++
@@ -1,7 +0,0 @@
-import LobbyTemplate from '@/components/templates/LobbyTemplate'
-import PlaygroundTemplate from '@/components/templates/PlaygroundTemplate'
-import React from 'react'
-
-export default function page() {
-  return <PlaygroundTemplate />
-}
--- src/app/room/[id]/result/page.tsx
+++
@@ -1,6 +0,0 @@
-import ResultTemplate from '@/components/templates/ResultTemplate'
-import React from 'react'
-
-export default function page() {
-  return <ResultTemplate />
-}
